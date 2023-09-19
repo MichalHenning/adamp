57c5f7e63bd550991e8c74ad2bcc5103eaa19f14
Michal Henning <michalhenning@gmail.com>
2023-09-19 13:24:17 +0200

    dodanie comparevalues

--- prodzekt/src/com/company/Compare.java
+++ prodzekt/src/com/company/Compare.java
@@ -1,4 +1,59 @@
 package com.company;
 
 public class Compare {
+    public void CompareValues(int a, int b){
+        if(a == b){
+            System.out.println(a  + " = " + b);
+        }
+        if(a < b){
+            System.out.println(b + "jest wieksze od " + a);
+        }
+        if(a > b){
+            System.out.println(b + "jest mniejsze od " + a);
+        }
+    }
+    public void CompareValues(float a, float b){
+        if(a == b){
+            System.out.println(a  + " = " + b);
+        }
+        if(a < b){
+            System.out.println(b + "jest wieksze od " + a);
+        }
+        if(a > b){
+            System.out.println(b + "jest mniejsze od " + a);
+        }
+    }
+    public void CompareValues(double a, double b){
+        if(a == b){
+            System.out.println(a  + " = " + b);
+        }
+        if(a < b){
+            System.out.println(b + "jest wieksze od " + a);
+        }
+        if(a > b){
+            System.out.println(b + "jest mniejsze od " + a);
+        }
+    }
+    public void CompareValues(String a, String b){
+        if(a == b){
+            System.out.println(a  + " = " + b);
+        }
+        if(a < b){
+            System.out.println(b + "jest wieksze od " + a);
+        }
+        if(a > b){
+            System.out.println(b + "jest mniejsze od " + a);
+        }
+    }
+    public void CompareValues(byte a, byte b){
+        if(a == b){
+            System.out.println(a  + " = " + b);
+        }
+        if(a < b){
+            System.out.println(b + "jest wieksze od " + a);
+        }
+        if(a > b){
+            System.out.println(b + "jest mniejsze od " + a);
+        }
+    }
 }
