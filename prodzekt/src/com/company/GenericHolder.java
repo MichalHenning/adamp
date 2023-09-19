fe1d74493dc657a886df4bc8c443a523ba6695c8
Michal Henning <michalhenning@gmail.com>
2023-09-19 12:50:49 +0200

    commit2

---
+++ prodzekt/src/com/company/GenericHolder.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class GenericHolder <AnyType>{
+
+    private AnyType a;
+
+    public AnyType getA() {
+        return a;
+    }
+    public void SetA(AnyType a) {
+        this.a = a;
+    }
+
+
+}
+
+
