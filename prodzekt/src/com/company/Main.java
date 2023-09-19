fff7716749e46afc2d14f2c775ee4ef49fa593e4
Michal Henning <michalhenning@gmail.com>
2023-09-19 12:12:54 +0200

    test1

--- prodzekt/src/com/company/Main.java
+++ prodzekt/src/com/company/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
         LiczbaHolder lh1 = new LiczbaHolder(19);
         LiczbaHolder lh2 = new LiczbaHolder(44  );
+        System.out.println("dsafgdsa");
+        System.out.println("fsdhgdskjgdjksjgjdshkugdujs");
+        System.out.println("fsdhgdskjgdjksjgjdshkugdujs");
+
     }
 }
