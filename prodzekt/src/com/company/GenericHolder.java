9bef6649088c1607442f669a26a7898dfe581c72
Michal Henning <michalhenning@gmail.com>
2023-09-19 13:16:04 +0200

    utworzenie klas

--- prodzekt/src/com/company/GenericHolder.java
+++ prodzekt/src/com/company/GenericHolder.java
@@ -4,14 +4,19 @@ public class GenericHolder <AnyType>{
 
     private AnyType a;
 
-    public AnyType getA() {
-        return a;
-    }
-    public void SetA(AnyType a) {
+    public GenericHolder(AnyType a) {
         this.a = a;
     }
 
+    public AnyType getA() {
+        return a;
+    }
+
+    public void setA(AnyType a) {
+        this.a = a;
+    }
 
 }
+
 
 
--- prodzekt/src/com/company/Main.java
+++ prodzekt/src/com/company/Main.java
@@ -1,13 +1,24 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
         LiczbaHolder lh1 = new LiczbaHolder(19);
         LiczbaHolder lh2 = new LiczbaHolder(44  );
-        System.out.println("dsafgdsa");
-        System.out.println("fsdhgdskjgdjksjgjdshkugdujs");
-        System.out.println("fsdhgdskjgdjksjgjdshkugdujs");
+        System.out.println(lh1.getA());
+        lh1.setA(10);
+        System.out.println(lh1.getA());
 
+        GenericHolder<Integer> gh1 = new GenericHolder(12);
+        System.out.println(gh1.getA());
+        gh1.setA(2);
+        System.out.println(gh1.getA());
+
+
+        ObjectHolder oh1 = new ObjectHolder(new Car());
+        System.out.println(oh1.getA());
+        oh1.setA(new Car()  );
+        System.out.println((int)oh1.getA()+1);
     }
 }
